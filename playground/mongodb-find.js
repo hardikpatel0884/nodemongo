c6ca6de17194de0c056ac0c3355eb8ae23e68291
Hardik <hardikpatel0884@gmail.com>
2017-12-30 11:40:36 +0530

    node mongo api demo

---
+++ playground/mongodb-find.js
@@ -0,0 +1,29 @@
+// const MongoClient=require('mongodb').MongoClient;
+const {MongoClient,ObjectID}=require('mongodb');
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    const db=client.db("TodoApp");
+
+    //fetch records .find({key:value})
+//    db.collection('Todos').find({_id:new ObjectID('5a47287c8560f425c54ba15d')}).toArray().then((docs)=>{
+//         console.log('Todos');
+//         console.log(JSON.stringify(docs,undefined,2));
+//    },(err)=>{
+//        console.log('Unable to fetch todos ',err);
+//    });
+
+    //getting count of records
+    db.collection('Todos').find().count().then((count)=>{
+        console.log(`${count} found`);
+        
+    },(err)=>{
+    console.log('Unable to fetch todos ',err);
+    });
+
+
+   client.close();
+});
